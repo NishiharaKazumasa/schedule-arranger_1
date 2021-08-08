7c87b5868aad1777e86b20f52e35319bfeb300e3
NishiharaKazumasa <Iru.h1991@gmail.com>
2021-08-08 23:54:10 +0900

    Herokuの環境変数を修正

--- app.js
+++ app.js
@@ -26,8 +26,8 @@ User.sync().then(() => {
 });
 
 var GitHubStrategy = require('passport-github2').Strategy;
-var GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID || '2f831cb3d4aac02393aa';
-var GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET || '9fbc340ac0175123695d2dedfbdf5a78df3b8067';
+var GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID || 'fe63d2653b3f7c24b83c';
+var GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET || 'b1c8a19b4edd704a67575cb064fafc7157bcb671';
 
 passport.serializeUser(function (user, done) {
   done(null, user);
